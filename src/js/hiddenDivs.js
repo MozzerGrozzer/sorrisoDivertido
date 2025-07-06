9d4368851dfdd8e65dd3222251809cf558e6977f
MozzerGrozzer <grozzermozzer@gmail.com>
2025-07-06 09:51:04 -0300

    Fix: javascript(#27)

--- src/js/hiddenDivs.js
+++ src/js/hiddenDivs.js
@@ -1,6 +1,6 @@
 function showSection(divId) {
       // update divs when add
-      const divs = ['div1', 'div2', 'div3'];
+      const divs = ['div1', 'div2', 'div3', 'div4', 'div5', 'div6'];
       divs.forEach(id => {
         document.getElementById(id).className = 'hidden';
       });
